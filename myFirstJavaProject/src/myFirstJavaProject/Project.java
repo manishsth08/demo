3b523b024e6816e7753b8d2d6b0ec57114be6129
Manish Shrestha <manish@prabinas-mbp>
2023-06-06 23:18:44 -0400

    added 123

--- myFirstJavaProject/src/myFirstJavaProject/Project.java
+++ myFirstJavaProject/src/myFirstJavaProject/Project.java
@@ -3,7 +3,7 @@ package myFirstJavaProject;
 public class Project {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World 123");
 
 	}
 
